52c2548cba6025abd8bf912a376b88e964c860b4
Antonio Vitor <nP_vitor@hotmail.com>
2020-06-01 17:51:35 -0300

    Atualizacao

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,22 @@
 import React, {useState} from 'react';
 import axios from 'axios';
 import * as S from './styled';
+import { useHistory } from 'react-router-dom';
 
 function Home(props) {
+  const history = useHistory();
   const [ usuario,setUsuario ] = useState('');
 
   function handlePesquisa(){
-    axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => console.log(response.data));
+    axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => {
+      const repositories = response.data;
+      const repositoriesName = [];
+      repositories.map((repository) => {
+        repositoriesName.push(repository.name);
+      });
+      localStorage.setItem('repositoriesName', JSON.stringify(repositoriesName));
+      history.push('/repositories')
+    });
   }
 
   return (
--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -1,21 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import * as S from './styled';
+
 export default function Repositories(){
+   const [ repositories, setRepositories ] = useState([]);
+
+   useEffect(() => {
+      let respositoriesName = localStorage.getItem('repositoriesName');
+      respositoriesName = JSON.parse(respositoriesName);
+      setRepositories(respositoriesName);
+   }, []);
+   
    return (
       <S.Container>
          <S.Title>Repositórios</S.Title>
          <S.List>
-            <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-            <S.ListItem>Repositório: Nome Repositório </S.ListItem>
-            <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-            <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-            <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-            <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-            <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-            <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-            <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-            <S.ListItem>Repositório: Nome Repositório</S.ListItem>
+            { repositories.map(repository => {
+               return(
+               <S.ListItem>Repositório: {repository}</S.ListItem>
+               )
+            })}
          </S.List>
+         <S.LinkHome to='/'>Voltar</S.LinkHome>
       </S.Container>
     );
 }
