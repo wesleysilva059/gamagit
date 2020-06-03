0ef3ffc45011a6ebe046130cf4d8bdeae3561a1c
Antonio Vitor <nP_vitor@hotmail.com>
2020-06-03 09:14:31 -0300

    atualização

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,24 +6,34 @@ import { useHistory } from 'react-router-dom';
 function Home(props) {
   const history = useHistory();
   const [ usuario,setUsuario ] = useState('');
+  const [ erro, setErro] = useState(false);
 
   function handlePesquisa(){
-    axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => {
+    axios.get(`https://api.github.com/users/${usuario}/repos`)
+    .then(response => {
       const repositories = response.data;
       const repositoriesName = [];
       repositories.map((repository) => {
         repositoriesName.push(repository.name);
       });
       localStorage.setItem('repositoriesName', JSON.stringify(repositoriesName));
+      setErro(false);
       history.push('/repositories')
+    })
+    .catch(err => {
+      setErro(true);
     });
   }
 
   return (
-    <S.Container>
-      <S.Input className="usuarioinput" placeholder="Usuário" value={usuario} onChange={e => setUsuario(e.target.value)}/>
-      <S.Button type="button" onClick={handlePesquisa}>Pesquisar</S.Button>
-    </S.Container>
+    <S.HomeContainer>
+      <S.Content>
+        <S.Input className="usuarioinput" placeholder="Usuário" value={usuario} onChange={e => setUsuario(e.target.value)}/>
+        <S.Button type="button" onClick={handlePesquisa}>Pesquisar</S.Button>
+      </S.Content>
+      { erro ? <S.ErrorMsg>Ocorreu um erro. Tente novamente.</S.ErrorMsg> : '' }
+      
+    </S.HomeContainer>
   );
 }
 
